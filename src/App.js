9dbe53497547e56cd71380bb5fad365c23a6b4ec
Eric Liu <eric@nomoss.co>
2024-11-08 18:37:04 +1100

    feat: add problem-focused view

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
+import ProblemsTable from "./ProblemsTable";
 
 const baseUrl = 'https://firestore.googleapis.com/v1/projects/fingercomps-lite-au/databases/(default)/documents';
 
@@ -13,12 +14,14 @@ function App() {
   const [selectedCompId, setSelectedCompId] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("");
   const [selectedCategoryCode, setSelectedCategoryCode] = useState("");
-  const [tableData, setTableData] = useState([]);
+  const [userTableData, setUserTableData] = useState([]);
   const [expandedRows, setExpandedRows] = useState(new Set());
   const [limitScores, setLimitScores] = useState(false);
   const [loading, setLoading] = useState(true);
   const [sortDirection, setSortDirection] = useState('desc');
   const [lastSubmittedScore, setLastSubmittedScore] = useState(null);
+  const [focusView, setFocusView] = useState('user');
+  const [minScoresToCount, setMinScoresToCount] = useState(1); // TODO initialise to 50% pumpfestTopScores
 
   const twoMonthsAgoISOString = () => {
     const now = new Date();
@@ -74,7 +77,7 @@ function App() {
       if (!s) return sum;
       tops += 1;
       if (s.flashed) flashes += 1;
-      return sum + (parseInt(s.score) + (s.flashed ? flashPoints : 0));
+      return sum + (s.score + (s.flashed ? flashPoints : 0));
     }, 0);
     return { tops, flashes, total };
   };
@@ -123,9 +126,9 @@ function App() {
               name: item.fields?.name?.stringValue,
               code: item.fields?.code?.stringValue,
               disableFlash: item.fields?.disableFlash?.booleanValue,
-              flashExtraPoints: item.fields?.flashExtraPoints?.integerValue,
+              flashExtraPoints: parseInt(item.fields?.flashExtraPoints?.integerValue),
               scalePoints: item.fields?.scalePoints?.booleanValue,
-              pumpfestTopScores: item.fields?.pumpfestTopScores?.integerValue,
+              pumpfestTopScores: parseInt(item.fields?.pumpfestTopScores?.integerValue),
             };
             return acc;
           }, {});
@@ -140,10 +143,10 @@ function App() {
         (documents) => {
           const cleanedCompetitors = documents.reduce((acc, item) => {
             const c = item.fields;
-            acc[c?.competitorNo?.integerValue] = {
+            acc[parseInt(c?.competitorNo?.integerValue)] = {
               name: `${c?.firstName?.stringValue.trim()} ${c?.lastName?.stringValue.trim()}`,
               category: c?.category?.stringValue,
-              competitorNo: c?.competitorNo?.integerValue,
+              competitorNo: parseInt(c?.competitorNo?.integerValue),
             };
             return acc;
           }, {});
@@ -160,7 +163,7 @@ function App() {
             const competitorNo = item.fields?.competitorNo?.integerValue;
             acc[competitorNo] = acc[competitorNo] || [];
             acc[competitorNo].push({
-              climbNo: item.fields?.climbNo?.integerValue,
+              climbNo: parseInt(item.fields?.climbNo?.integerValue),
               category: item.fields?.category?.stringValue,
               flashed: item.fields?.flash?.booleanValue,
               topped: item.fields?.topped?.booleanValue,
@@ -180,10 +183,11 @@ function App() {
         (documents) => {
           const cleanedProblems = documents.reduce((acc, item) => {
             acc[item.fields?.climbNo?.integerValue] = {
-              score: item.fields?.score?.integerValue,
+              score: parseInt(item.fields?.score?.integerValue),
               station: item.fields?.station?.stringValue,
               marking: item.fields?.marking?.stringValue,
-              climbNo: item.fields?.climbNo?.integerValue,
+              climbNo: parseInt(item.fields?.climbNo?.integerValue),
+              createdAt: item.fields?.created?.timestampValue,
             };
             return acc;
           }, {});
@@ -202,10 +206,10 @@ function App() {
   }, [selectedCompId]);
 
   useEffect(() => {
-    const computeTableData = () => {
+    const computeUserTableData = () => {
       const table = Object.entries(competitors).map(([uid, user]) => {
         const cat = categories[user.category];
-        const flashExtraPoints = parseInt(cat?.flashExtraPoints || 0);
+        const flashExtraPoints = cat?.flashExtraPoints || 0;
         const row = {
           ...user,
           scores: (scores[uid] || [])
@@ -219,7 +223,7 @@ function App() {
         const { tops, flashes, total } = computeScore(
           row.scores,
           flashExtraPoints,
-          parseInt(cat?.pumpfestTopScores || 0)
+          cat?.pumpfestTopScores || 0
         );
         return {
           ...row,
@@ -244,22 +248,69 @@ function App() {
       });
 
       return table;
+    }
+    const computeProblemStats = () => {
+      const seen = new Set();
+      Object.entries(scores).forEach(([cptNo, cptScores]) => {
+        cptScores.forEach((score) => {
+          const tmpId = `${cptNo},${score.climbNo}`;
+          // Skip duplicates
+          if (seen.has(tmpId)) {
+            console.warn(`Duplicate climb detected: ${tmpId}`);
+            return;
+          } else {
+            seen.add(tmpId);
+          }
+          const climb = problems[score.climbNo];
+          if (!climb.stats) {
+            climb['stats'] = {};
+            Object.keys(categories).forEach((category) => {
+              climb.stats[category] = {
+                'tops': 0,
+                'flashes': 0,
+              }
+            });
+          }
+          if (score.flashed) {
+            climb.stats[score.category].flashes += 1;
+          }
+          if (score.topped) {
+            climb.stats[score.category].tops += 1;
+          }
+        });
+      });
     };
-    setTableData(computeTableData());
+    setUserTableData(computeUserTableData());
+    computeProblemStats();
   }, [categories, competitors, problems, scores]);
+
+  const computeCategoryTops = () => {
+    const categoryTops = {};
+    Object.keys(categories).forEach((category) => {
+      categoryTops[category] = [];
+    });
+    Object.values(scores).forEach((competitorScores) => {
+      // Assume competitor is just in one category and add their score
+      if (competitorScores.length > 0) {
+        categoryTops[competitorScores[0].category]?.push(competitorScores.length);
+      }
+    });
+    return categoryTops;
+  };
+  const categoryTops = useMemo(computeCategoryTops, [categories, scores]);
 
   const handleScoreHeaderClick = () => {
     // Toggle sort direction
     const newSortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
     setSortDirection(newSortDirection);
 
-    // Sort the tableData immediately based on the new sort direction
-    const sortedTableData = [...tableData].sort((a, b) =>
+    // Sort the userTableData immediately based on the new sort direction
+    const sortedUserTableData = [...userTableData].sort((a, b) =>
       newSortDirection === 'asc' ? a.total - b.total : b.total - a.total
     );
 
-    // Update tableData directly with sorted data
-    setTableData(sortedTableData);
+    // Update userTableData directly with sorted data
+    setUserTableData(sortedUserTableData);
   };
 
   const handleRowClick = (uid) => {
@@ -286,6 +337,11 @@ function App() {
     setExpandedRows(new Set()); // Collapse expanded rows when changing category
   }, [scores, selectedCategoryCode]);
 
+  const countCompetitors = (category) => {
+    const count = categoryTops[category].reduce((acc, curr) => (curr >= minScoresToCount) ? acc + 1 : acc, 0);
+    return count/100;
+  };
+
   return (
     <div className="App">
       <h1>FingerComps Extended</h1>
@@ -305,6 +361,24 @@ function App() {
               {item.document.fields?.name?.stringValue}
             </option>
           ))}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="focusView">Focus on:</label>
+        <select
+          id="focusView"
+          value={focusView}
+          onChange={(e) => {
+            setFocusView(e.target.value);
+          }}
+          disabled={loading}
+        >
+          <option value="user">
+            Users
+          </option>
+          <option value="problems">
+            Problems
+          </option>
         </select>
       </div>
       <div>
@@ -341,87 +415,98 @@ function App() {
       {/* Loading message */}
       {loading && <p>Loading data, please wait...</p>}
       {/* Table of competitors and scores */}
-      <table border="1">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Overall Rank</th>
-            {!selectedCategory ? (<th>Category</th>) : null}
-            <th>Name</th>
-            <th>Tops</th>
-            <th>Flashes</th>
-            <th onClick={handleScoreHeaderClick} style={{ cursor: 'pointer' }}>
-              Score (+ Flash Bonus) {sortDirection === 'asc' ? '🔼' : '🔽'}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {loading ? (
+      {focusView === 'user' ? (
+        <table border="1">
+          <thead>
             <tr>
-              <td colSpan="10">Loading...</td>
+              <th>#</th>
+              <th>Overall Rank</th>
+              {!selectedCategory ? (<th>Category</th>) : null}
+              <th>Name</th>
+              <th>Tops</th>
+              <th>Flashes</th>
+              <th onClick={handleScoreHeaderClick} style={{ cursor: 'pointer' }}>
+                Score (+ Flash Bonus) {sortDirection === 'asc' ? '🔼' : '🔽'}
+              </th>
             </tr>
-          ) : Array.isArray(tableData) && tableData.length > 0 ? (
-            tableData
-              .filter(item => {
-                if (!selectedCategory) return true;
-                return item.categoryFullName === selectedCategory;
-              })
-              .map((item, index) => {
-                const isExpanded = expandedRows.has(item.competitorNo);
-                return (
-                  <React.Fragment key={index}>
-                    <tr onClick={() => handleRowClick(item.competitorNo)} style={{ cursor: 'pointer' }}>
-                      <td>{isNaN(index) ? 'N/A' : index + 1}</td>
-                      <td>{item.rank}</td>
-                      {!selectedCategory ? (<td>{item.categoryFullName}</td>) : null}
-                      <td>{item.name}</td>
-                      <td>{item.tops}</td>
-                      <td>{item.flashes}</td>
-                      <td>{item.total - item.bonus} {item.bonus > 0 ? `(+${item.bonus})` : ''}</td>
-                    </tr>
-                    {isExpanded && (
-                      <tr>
-                        <td colSpan="7">
-                          {/* Subtable */}
-                          <table border="1" style={{ width: '100%' }}>
-                            <thead>
-                              <tr>
-                                <th>Problem No.</th>
-                                <th>Name/Grade</th>
-                                <th>Flashed?</th>
-                                <th>Points (+ Flash Bonus)</th>
-                                <th>Sent</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              {/* Limit the number of scores based on the checkbox state */}
-                              {item.scores && item.scores
-                                .slice(0, limitScores ? categories[item.category].pumpfestTopScores : item.scores.length)
-                                .map((score, subIndex) =>
-                              (
-                                <tr key={subIndex}>
-                                  <td>{score.climbNo}</td>
-                                  <td>{score.marking}</td>
-                                  <td>{score.flashed ? 'Y' : ''}</td>
-                                  <td>{score.score} {score.flashed ? `(+${item.flashExtraPoints})` : ''}</td>
-                                  <td>{toTimeAgoString(score.createdAt)}</td>
-                                </tr>
-                              ))}
-                            </tbody>
-                          </table>
-                        </td>
+          </thead>
+          <tbody>
+            {loading ? (
+              <tr>
+                <td colSpan="10">Loading...</td>
+              </tr>
+            ) : Array.isArray(userTableData) && userTableData.length > 0 ? (
+              userTableData
+                .filter(item => {
+                  if (!selectedCategory) return true;
+                  return item.categoryFullName === selectedCategory;
+                })
+                .map((item, index) => {
+                  const isExpanded = expandedRows.has(item.competitorNo);
+                  return (
+                    <React.Fragment key={index}>
+                      <tr onClick={() => handleRowClick(item.competitorNo)} style={{ cursor: 'pointer' }}>
+                        <td>{isNaN(index) ? 'N/A' : index + 1}</td>
+                        <td>{item.rank}</td>
+                        {!selectedCategory ? (<td>{item.categoryFullName}</td>) : null}
+                        <td>{item.name}</td>
+                        <td>{item.tops}</td>
+                        <td>{item.flashes}</td>
+                        <td>{item.total - item.bonus} {item.bonus > 0 ? `(+${item.bonus})` : ''}</td>
                       </tr>
-                    )}
-                  </React.Fragment>
-                );
-              })
-          ) : (
-            <tr>
-              <td colSpan="10">No data available</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+                      {isExpanded && (
+                        <tr>
+                          <td colSpan="7">
+                            {/* Subtable */}
+                            <table border="1" style={{ width: '100%' }}>
+                              <thead>
+                                <tr>
+                                  <th>Problem No.</th>
+                                  <th>Name/Grade</th>
+                                  <th>Flashed?</th>
+                                  <th>Points (+ Flash Bonus)</th>
+                                  <th>Sent</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                {/* Limit the number of scores based on the checkbox state */}
+                                {item.scores && item.scores
+                                  .slice(0, limitScores ? categories[item.category].pumpfestTopScores : item.scores.length)
+                                  .map((score, subIndex) =>
+                                (
+                                  <tr key={subIndex}>
+                                    <td>{score.climbNo}</td>
+                                    <td>{score.marking}</td>
+                                    <td>{score.flashed ? 'Y' : ''}</td>
+                                    <td>{score.score} {score.flashed ? `(+${item.flashExtraPoints})` : ''}</td>
+                                    <td>{toTimeAgoString(score.createdAt)}</td>
+                                  </tr>
+                                ))}
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                      )}
+                    </React.Fragment>
+                  );
+                })
+            ) : (
+              <tr>
+                <td colSpan="10">No data available</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      ) : (
+        <ProblemsTable 
+          categories={categories} 
+          categoryTops={categoryTops} 
+          problems={problems} 
+          loading={loading} 
+          countCompetitors={countCompetitors} 
+          toTimeAgoString={toTimeAgoString} 
+        />
+      )}
       {/* Last score date display */}
       <div style={{ marginTop: '20px' }}>
         {lastSubmittedScore ? (
