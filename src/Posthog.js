584aa228a95fe9692d8ba262a9250623c564498b
Eric Liu <eric@nomoss.co>
2025-02-21 11:41:03 +1100

    chore: disable posthog in local dev

--- src/Posthog.js
+++ src/Posthog.js
@@ -1,12 +1,15 @@
 import posthog from 'posthog-js'
 
 const loadPosthog = () => {
-    posthog.init('phc_gaex6n8piCE8xkDEq6IPAmvkNBFinGBZkilZXfsuTnG',
-        {
-            api_host: 'https://us.i.posthog.com',
-            person_profiles: 'always' // or 'always' to create profiles for anonymous users as well
-        }
-    )
+    // Skip posthog for local
+    if (process.env.NODE_ENV !== 'development') {
+        posthog.init('phc_gaex6n8piCE8xkDEq6IPAmvkNBFinGBZkilZXfsuTnG',
+            {
+                api_host: 'https://us.i.posthog.com',
+                person_profiles: 'always' // or 'always' to create profiles for anonymous users as well
+            }
+        )
+    }
 }
 
 export default loadPosthog;
