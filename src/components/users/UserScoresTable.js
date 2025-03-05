7832cd2e078300b83b66362737927cf9e061c2d9
Eric Liu <eric@nomoss.co>
2025-03-06 06:55:51 +1100

    style: resolve extra borders appearing when using display flex

--- src/components/users/UserScoresTable.js
+++ src/components/users/UserScoresTable.js
@@ -81,7 +81,7 @@ function UserScoresTable({
                   onClick={() => toggleRow(score.climbNo)}
                   style={shouldHighlight(score.climbNo) ? { backgroundColor: '#E9FFDB'  } : {backgroundColor: '#F9F9F9' }}
                 >
-                  <td className="center">
+                  <td>
                     {shouldHighlight(score.climbNo) ? '✅' : ''}
                     {score.climbNo}
                     <PhotoIndicator
